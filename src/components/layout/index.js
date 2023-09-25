72cc0aa83b3a8c1853387090d94e5f7acf04e6d9
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-25 14:12:47 +0500

    create layout page

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import "./index.css"
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      <div className = "layout">
+        <Link to="/">Home</Link>
+        <Link to="/allProducts">AllProducts</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/account">Account</Link>
+      </div>
+      <Outlet />
+    </div>
   )
 }
 
