3a16a9ef38a29e4a4ab56082b9d0c320de66121c
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-25 17:02:38 +0500

    create user function by firebase

--- src/pages/account/myAccount/index.js
+++ src/pages/account/myAccount/index.js
@@ -1,39 +1,47 @@
 import React from 'react'
 import Form from '../../../components/form'
 // firebase
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-const auth = getAuth();
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseApp } from './Firebase';
 
-const signUpUser = (firstName,lastName,email, password) => {
-  createUserWithEmailAndPassword(auth, email, password,firstName,lastName)
-    .then((userCredential) => {
-      const user = userCredential.user;
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-    const firebaseConfig = {
-      apiKey: "AIzaSyAW97qIZtX0VZjzb6Y2xy-Gd3lAA-EIq8g",
-      authDomain: "ecommerce-websites-60755.firebaseapp.com",
-      projectId: "ecommerce-websites-60755",
-      storageBucket: "ecommerce-websites-60755.appspot.com",
-      messagingSenderId: "789140843562",
-      appId: "1:789140843562:web:14363ba61243696f73e753",
-      measurementId: "G-5N5EC82G0J"
-    };
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
+const auth = getAuth(firebaseApp);
+
+const signUpUser = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode)
+    console.log(errorMessage)
+  });
+
+}
+const signInUser = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode)
+    console.log(errorMessage)
+  });
+
 }
 
 
 const MyAccount = () => {
 
   return (
-   <Form signIn  onFormSubmit = {signUpUser}></Form>
+   <Form signIn  onFormSubmit = {signInUser} ></Form>
   )
 }
 
