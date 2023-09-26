7a5a28e7ccab0054fe1baf078b023e417b38bdbc
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-26 16:34:23 +0500

    update form file

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -2,27 +2,28 @@ import { useRef } from "react";
 import { Link } from "react-router-dom"
 
 
-export default function Form({signIn,onFormSubmit}) {
+export default function Form({signIn,onFormSubmitSignUp, onFormSubmitSignIn}) {
 const firstNameRef = useRef()
 const lastNameRef = useRef()
 const emailRef = useRef()
 const passwordRef = useRef()
+const imageUrlRef = useRef()
 
-const onSubmitHandler = (event) => {
+const onSubmitHandlerForSignUp = (event) => {
   event.preventDefault();
   const firstName = firstNameRef.current.value;
   const lastName = lastNameRef.current.value;
   const email = emailRef.current.value;
   const password = passwordRef.current.value;
-  onFormSubmit(email, password);
+  const imageUrl = imageUrlRef.current.value;
+  onFormSubmitSignUp(email, password);
 }
-const onSubmitHandler1 = (event) => {
+
+const onSubmitHandlerForSignIn = (event) => {
   event.preventDefault();
-  const firstName = firstNameRef.current.value;
-  const lastName = lastNameRef.current.value;
   const email = emailRef.current.value;
   const password = passwordRef.current.value;
-  onFormSubmit(email, password);
+  onFormSubmitSignIn(email, password);
 }
 
 
@@ -41,7 +42,7 @@ const onSubmitHandler1 = (event) => {
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6"  onSubmit={onSubmitHandler1}>
+            <form className="space-y-6" >
                 {signIn? "":
                 <>
                  <div>
@@ -118,12 +119,29 @@ const onSubmitHandler1 = (event) => {
                   />
                 </div>
               </div>
+              {signIn ? "":
+              <div>
+              <label htmlFor="imageUrl" className="block text-sm font-medium leading-6 text-gray-900">
+                ImageUrl
+              </label>
+              <div className="mt-2">
+                <input
+                  id="imageUrl"
+                  name="imageUrl"
+                  type="url"
+                  autoComplete="imageUrl"
+                  ref={imageUrlRef}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>}
   
             {signIn?  <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
+                  onClick={onSubmitHandlerForSignIn}>
                   Sign in
                 </button>
               </div>:
@@ -131,7 +149,7 @@ const onSubmitHandler1 = (event) => {
                <button
                  type="submit"
                  className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-               >
+                onClick={onSubmitHandlerForSignUp}>
                  Sign up
                </button>
              </div>
