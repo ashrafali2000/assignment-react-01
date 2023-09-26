aaa3bb54e10eece70828de099ea2d72acaf6c2be
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-26 16:34:02 +0500

    update indexjs file

--- src/pages/account/myAccount/index.js
+++ src/pages/account/myAccount/index.js
@@ -2,17 +2,25 @@ import React from 'react'
 import Form from '../../../components/form'
 // firebase
 
-// import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set  } from "firebase/database";
+
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import { firebaseApp } from './Firebase';
 
 const auth = getAuth(firebaseApp);
+const db = getDatabase();
 
-const signUpUser = (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
+const signUpUser = (firstName,lastName,email, password,imageUrl) => {
+  createUserWithEmailAndPassword(auth,firstName,lastName, email, password,imageUrl)
   .then((userCredential) => {
     const user = userCredential.user;
-    console.log(user)
+    set(ref(db, `users/${user.uid}`), {
+          username: firstName+lastName,
+          email: email,
+          password:password,
+          profile_picture : imageUrl
+        });
+
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -22,26 +30,55 @@ const signUpUser = (email, password) => {
   });
 
 }
-const signInUser = (email, password) => {
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user)
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
-  });
 
-}
+
+
+
+// Authentication
+
+// import { getAnalytics } from "firebase/analytics";
+
+// import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+// import { firebaseApp } from './Firebase';
+
+// const auth = getAuth(firebaseApp);
+// const auth1 = getAuth();
+
+// const signUpUser = (email, password) => {
+//   createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     const user = userCredential.user;
+//     console.log(user)
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.log(errorCode)
+//     console.log(errorMessage)
+//   });
+
+// }
+
+// const signInUser = (email, password) => {
+//   signInWithEmailAndPassword(auth1, email, password)
+//   .then((userCredential) => {
+//     const user = userCredential.user;
+//     console.log(user)
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.log(errorCode)
+//     console.log(errorMessage)
+//   });
+
+// }
 
 
 const MyAccount = () => {
 
   return (
-   <Form signIn  onFormSubmit = {signInUser} ></Form>
+   <Form signI  onFormSubmitSignUp = {signUpUser}  onFormSubmitSignIn></Form>
   )
 }
 
