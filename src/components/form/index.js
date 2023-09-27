2d9432cbbbea5864ca9ea462dc92badedf5fdb7d
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-27 14:50:55 +0500

    some changes in form file

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,182 +1,228 @@
 import { useRef, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-export default function Form({signIn,onFormSubmitSignUp, onFormSubmitSignIn}) {
+export default function Form({
+  signIn,
+  onFormSubmitSignUp,
+  onFormSubmitSignIn,
+}) {
   const [signInShow, setSignInShow] = useState(signIn);
 
-const firstNameRef = useRef()
-const lastNameRef = useRef()
-const emailRef = useRef()
-const passwordRef = useRef()
-const imageUrlRef = useRef()
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const imageUrlRef = useRef();
 
-const onSubmitHandlerForSignUp = (event) => {
-  event.preventDefault();
-  const firstName = firstNameRef.current.value;
-  const lastName = lastNameRef.current.value;
-  const email = emailRef.current.value;
-  const password = passwordRef.current.value;
-  const imageUrl = imageUrlRef.current.value;
-  onFormSubmitSignUp(firstName, lastName, email, password, imageUrl);
-}
+  const onSubmitHandlerForSignUp = (event) => {
+    event.preventDefault();
+    const firstName = firstNameRef.current.value;
+    const lastName = lastNameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const imageUrl = imageUrlRef.current.value;
+    onFormSubmitSignUp(firstName, lastName, email, password, imageUrl);
+  };
 
-const onSubmitHandlerForSignIn = (event) => {
-  event.preventDefault();
-  const email = emailRef.current.value;
-  const password = passwordRef.current.value;
-  onFormSubmitSignIn(email, password);
-}
+  const onSubmitHandlerForSignIn = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    onFormSubmitSignIn(email, password);
+  };
 
-const formShowSignUpHandler = () => {
-  setSignInShow("");
-}
+  const formShowSignUpHandler = () => {
+    setSignInShow("");
+  };
 
-const formShowSignINHandler = () => {
-  setSignInShow("SignIn");
-}
+  const formShowSignINHandler = () => {
+    setSignInShow("SignIn");
+  };
 
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            {signInShow ? " Sign in to your account": "Sign up for new Account"}
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" >
-                {signInShow? "":
-                <>
-                 <div>
-                 <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
-                  First Name
-                 </label>
-                 <div className="mt-2">
-                   <input
-                     id="firstName"
-                     name="firstName"
-                     type="text"
-                     autoComplete="firstName"
-                     ref={firstNameRef}
-                     required
-                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                   />
-                 </div>
-               </div>
-                 <div>
-                 <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
-                  Last Name
-                 </label>
-                 <div className="mt-2">
-                   <input
-                     id="lastName"
-                     name="lastName"
-                     type="text"
-                     autoComplete="lastName"
-                     ref={lastNameRef}
-                     required
-                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                   />
-                 </div>
-               </div>
-               </>
-               }
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    ref={emailRef}
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            {signInShow
+              ? " Sign in to your account"
+              : "Sign up for new Account"}
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6">
+            {signInShow ? (
+              ""
+            ) : (
+              <>
+                <div>
+                  <label
+                    htmlFor="firstName"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    First Name
                   </label>
-                  {signInShow ? <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>: ""}
+                  <div className="mt-2">
+                    <input
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      autoComplete="firstName"
+                      ref={firstNameRef}
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    ref={passwordRef}
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+                <div>
+                  <label
+                    htmlFor="lastName"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Last Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      autoComplete="lastName"
+                      ref={lastNameRef}
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
-              </div>
-              {signInShow ? "":
-              <div>
-              <label htmlFor="imageUrl" className="block text-sm font-medium leading-6 text-gray-900">
-                ImageUrl
+              </>
+            )}
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
               </label>
               <div className="mt-2">
                 <input
-                  id="imageUrl"
-                  name="imageUrl"
-                  type="url"
-                  autoComplete="imageUrl"
-                  ref={imageUrlRef}
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  ref={emailRef}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-            </div>}
-  
-            {signInShow?  <div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                {signInShow ? (
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  ref={passwordRef}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            {signInShow ? (
+              ""
+            ) : (
+              <div>
+                <label
+                  htmlFor="imageUrl"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  ImageUrl
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="imageUrl"
+                    name="imageUrl"
+                    type="url"
+                    autoComplete="imageUrl"
+                    ref={imageUrlRef}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            )}
+
+            {signInShow ? (
+              <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  onClick={onSubmitHandlerForSignIn}>
+                  onClick={onSubmitHandlerForSignIn}
+                >
                   Sign in
                 </button>
-              </div>:
-               <div>
-               <button
-                 type="submit"
-                 className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={onSubmitHandlerForSignUp}>
-                 Sign up
-               </button>
-             </div>
-              }
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              {signInShow ? <Link href="/" onClick={formShowSignUpHandler} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              </div>
+            ) : (
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={onSubmitHandlerForSignUp}
+                >
+                  Sign up
+                </button>
+              </div>
+            )}
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            {signInShow ? (
+              <Link
+                href="/"
+                onClick={formShowSignUpHandler}
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
                 Sign Up
-              </Link>:
-              <Link href="/" onClick={formShowSignINHandler} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              </Link>
+            ) : (
+              <Link
+                href="/"
+                onClick={formShowSignINHandler}
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
                 Sign In
-              </Link>}
-            </p>
-          </div>
+              </Link>
+            )}
+          </p>
         </div>
-      </>
-    )
-  }
-  
+      </div>
+    </>
+  );
+}
