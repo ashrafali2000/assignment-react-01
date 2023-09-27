0b45e576dfb374f487a72496b7ded59c27e418ff
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-09-27 14:50:09 +0500

    add map mathods

--- src/pages/allProducts/index.js
+++ src/pages/allProducts/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import Products from "./allProducts.json"
+import Cards from './Cards'
 const AllProducts = () => {
+  const allProducts = Products.map((item) => <Cards key={item.id} 
+  item = {item.id} title ={item.title} category = {item.category} description = {item.description} /> )
   return (
-    <div>AllProducts</div>
+    <div>
+    
+      <div>
+     {allProducts}
+      </div>
+    </div>
   )
 }
 
