0a29eb30bf045edea81eddf4372143b24c973ab6
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-04 11:27:07 +0500

    addToCartBtn create and style

---
+++ src/components/addToCartBtn/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./index.css"
+const AddToCartBtn = () => {
+  return (
+    <button className='AddToCartBtn'>Add To Card</button>
+  )
+}
+
+export default AddToCartBtn;
