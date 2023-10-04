473b06e886537a71ba3c24571659f120f373bdc9
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-04 14:51:44 +0500

    useParams for dynamic route

--- src/pages/singleProductPage/index.js
+++ src/pages/singleProductPage/index.js
@@ -1,12 +1,24 @@
+import Card from 'antd/es/card/Card'
+import { Image } from 'antd'
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Products from "../allProducts/allProducts.json"
 
-const SingleProductPage = () => {
-    const params = useParams();
-    const {userName} = params;
+const SingleCard = ({}) => {
+  const params = useParams();
+  const {porductID} = params;
+  const singleProduct = Products.find(((item) => item.id === +porductID));
+  
+
+
   return (
-    <div>SingleProductPage {userName}</div>
+    <Card className="myCardCls" >
+     <Image src={singleProduct.images[1]} className='myCardImg'></Image>
+       <p>{singleProduct.title}</p>
+       <p>{singleProduct.category}</p> 
+      
+    </Card>
   )
 }
 
-export default SingleProductPage
+export default SingleCard
