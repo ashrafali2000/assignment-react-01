0c563da20988721a9153be6f5c68bfe896ce4bc6
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-04 11:30:20 +0500

    add img props

--- src/pages/allProducts/index.js
+++ src/pages/allProducts/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Products from "./allProducts.json"
 import Cards from './Cards'
+import "./index.css"
+import { Link } from 'react-router-dom'
 const AllProducts = () => {
   const allProducts = Products.map((item) => <Cards key={item.id} 
-  item = {item.id} title ={item.title} category = {item.category} description = {item.description} /> )
+  item = {item.id} title ={item.title} img ={item.images[1]} category = {item.category} description = {item.description} /> )
   return (
     <div>
-    
-      <div>
+    <nav className='categoriesCls'><Link>Phones</Link><Link>LapToop</Link>
+    <Link>Food</Link><Link>Perfume</Link><Link>Home-decoration</Link></nav>
+      <div className='allCardCls'>
      {allProducts}
       </div>
+
     </div>
   )
 }
