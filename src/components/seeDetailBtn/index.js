e3fae8d2e9d578039dc1a0391c2f56240e26fd8c
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-04 13:04:28 +0500

    see details btn

---
+++ src/components/seeDetailBtn/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./index.css"
+const SeeDetailBtn = () => {
+  return (
+    <button className='SeeDetailBtn'>See Details</button>
+  )
+}
+
+export default SeeDetailBtn
