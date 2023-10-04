95e1c966413ccb27e721215c29b23a9edca8efa9
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-04 15:15:42 +0500

    card single for single product

--- src/pages/singleProductPage/index.js
+++ src/pages/singleProductPage/index.js
@@ -3,6 +3,8 @@ import { Image } from 'antd'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import Products from "../allProducts/allProducts.json"
+import "./index.css"
+import AddToCartBtn from '../../components/addToCartBtn'
 
 const SingleCard = ({}) => {
   const params = useParams();
@@ -12,11 +14,12 @@ const SingleCard = ({}) => {
 
 
   return (
-    <Card className="myCardCls" >
-     <Image src={singleProduct.images[1]} className='myCardImg'></Image>
+    <Card className="singleCardDetails" >
+  <div className='imagesInDetails'>
+     {singleProduct.images.map(p =><Image src={p} className='singleCardImg'></Image>)}</div>
        <p>{singleProduct.title}</p>
        <p>{singleProduct.category}</p> 
-      
+      <AddToCartBtn></AddToCartBtn>
     </Card>
   )
 }
