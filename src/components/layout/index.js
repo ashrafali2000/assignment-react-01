2c4f532eb195acb1d8773a13681684891660a9e6
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-07 18:18:31 +0500

    useContext for userImg

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,8 +1,9 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Outlet, Link } from 'react-router-dom'
 import logo from "../../logo.png"
+import { AuthContext } from '../Context/AuthCtx'
 const navigation = [
   { name: 'Home', href: '/', current: true },
   { name: 'AllProducts', href: '/allProducts', current: false },
@@ -15,7 +16,12 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
 export default function Layout() {
+
+  const userImg = useContext(AuthContext);
+  const img = userImg.imgUrl;
+  
   return (
     <div>
     <Disclosure as="nav" className="bg-gray-700">
@@ -79,7 +85,7 @@ export default function Layout() {
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={img}
                         alt=""
                       />
                     </Menu.Button>
