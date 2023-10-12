8d2e20c433e5a1654b5bb8fa46058b4926bdd397
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-12 11:36:39 +0500

    add function in AuthContext

--- src/Context/AuthContext/index.js
+++ src/Context/AuthContext/index.js
@@ -3,8 +3,10 @@ import { createContext } from "react"
   export  const AuthContext = createContext({
        isLoggedIn: false,
         imgUrl:"",
+        userEmail:"",
         signInForImageHandler:() => {},
-        signInHandler:() => {}
+        signInHandler:() => {},
+        emailHandler:() => {}
     })
   
 
