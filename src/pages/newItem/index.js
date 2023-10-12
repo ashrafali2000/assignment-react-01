c762d4c6a9294e86adc6b99bcf3b7b4b1ea38031
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-12 09:47:01 +0500

    create newitem component

---
+++ src/pages/newItem/index.js
@@ -0,0 +1,112 @@
+import React, { useRef } from "react";
+
+const NewItem = () => {
+  const titleRef = useRef();
+  const priceRef = useRef();
+  const brandRef = useRef();
+
+  const addItemHandler = () => {
+    const title = titleRef.current.value;
+    const price = priceRef.current.value;
+    const brand = brandRef.current.value;
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Title
+            </label>
+            <div className="mt-2">
+              <input
+                id="title"
+                name="title"
+                type="text"
+                autoComplete="title"
+                ref={titleRef}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="Price"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Price
+            </label>
+            <div className="mt-2">
+              <input
+                id="Price"
+                name="Price"
+                type="number"
+                autoComplete="Price"
+                ref={priceRef}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="Brand"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Brand
+            </label>
+            <div className="mt-2">
+              <input
+                id="Brand"
+                name="Brand"
+                type="text"
+                autoComplete="Brand"
+                ref={brandRef}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="imagesUrl"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Images Url
+            </label>
+            <div className="mt-2 flex justify-center item-center">
+              <input
+                id="imagesUrl"
+                name="imagesUrl"
+                type="url"
+                autoComplete="imagesUrl"
+                ref={brandRef}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            <button className="rounded-md md:w-32 sm:w-16 bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Push image
+            </button>
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={addItemHandler}
+            >
+             Add Item
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default NewItem;
