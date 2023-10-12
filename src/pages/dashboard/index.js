615ca518aeaf2a6f62ea815ee670dcb14a112807
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-12 11:35:51 +0500

    useContext in Dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Card, Image } from 'antd'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../Context/AuthContext'
 
 const Dashboard = () => {
+  const authCtx = useContext(AuthContext);
+  const email = authCtx.userEmail;
+  const products = JSON.parse(localStorage.getItem(email));
+  const productImages = products.products;
+  console.log(productImages)
   return (
-    <div>Dashboard</div>
+    <div>
+      <Card>
+       {productImages.map(p => <Image key={Math.random()} src={p.brand}>
+  
+        </Image>)}
+      </Card>
+      
+    </div>
   )
 }
 
